9586a45b83bc9a15187b8143d25b503dc4683f21
Igor <97473595+IgorTsoy2022@users.noreply.github.com>
2025-03-02 13:57:18 +0300

    Add files via upload

--- m17_01-03.cpp
+++ m17_01-03.cpp
@@ -18,16 +18,16 @@ void print(const A & arr, size_t from, size_t to) {
 }
 
 template<typename T>
-void swap(T& a, T& b) {
-	T tmp = a;
-	a = b;
-	b = tmp;
+void swap(T * a, T * b) {
+	T tmp = *a;
+	*a = *b;
+	*b = tmp;
 }
 
-template<typename T, const size_t N>
-void reverse(T* p) {
-	for (int i = 0; i < N / 2; ++i) {
-		swap(*(p + i), *(p + N - 1 - i));
+template<typename T>
+void reverse(T* p, const size_t size) {
+	for (int i = 0; i < size / 2; ++i) {
+		swap(&(*(p + i)), &(*(p + size - 1 - i)));
 	}
 }
 
@@ -44,7 +44,7 @@ void prefix_func(const char* str, int (&z)[N]) {
 }
 
 template<size_t N>
-int first_kmp(const char* text, const char* str) {
+int find_first_kmp(const char* text, const char* str) {
 	int pf[N];
 	prefix_func(str, pf);
 
@@ -66,18 +66,47 @@ int first_kmp(const char* text, const char* str) {
 }
 
 template<size_t N>
-bool substr(const char* a, const char* b) {
+void substr_kmp(const char* a, const char* b) {
 	auto idx = first_kmp<N>(a, b);
 	if (idx > -1) {
 		std::cout << "String \"" << b << "\" is a substring of the string \"" << a << "\"\n";
 		std::cout << "and starts at position " << idx << " of the string.\n";
-		return true;
 	}
 	else {
 		std::cout << "String \"" << b << "\" isn't a substring of the string \"" << a << "\"\n";
-		return false;
 	}
+}
 
+int find_first_brutal(const char * a, const char * b) {
+
+    for (int i = 0; a[i] != '\0'; ++i) {
+        bool matched = true;
+        for (int a_i = i, b_i = 0;
+                b[b_i] != '\0';
+                ++a_i, ++b_i) {
+            if (a[a_i] == '\0' ||
+                 a[a_i] != b[b_i]) {
+                 matched = false;
+                 break;
+             }
+        }
+        if (matched) {
+        	return i;
+        }
+    }
+
+    return -1;
+}
+
+void substr_brutal(const char* a, const char* b) {
+	auto idx = find_first_brutal(a, b);
+	if (idx > -1) {
+		std::cout << "String \"" << b << "\" is a substring of the string \"" << a << "\"\n";
+		std::cout << "and starts at position " << idx << " of the string.\n";
+	}
+	else {
+		std::cout << "String \"" << b << "\" isn't a substring of the string \"" << a << "\"\n";
+	}
 }
 
 int main() {
@@ -86,7 +115,7 @@ int main() {
 	{
 		int a = 1, b = 2;
 		std::cout << "a = " << a << " b = " << b << "\n";
-		swap(a, b);
+		swap(&a, &b);
 		std::cout << "a = " << a << " b = " << b << "\n";
 	}
 
@@ -94,7 +123,7 @@ int main() {
 	{
 		int arr[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
 		print(arr, 0, 9);
-		reverse<int, 10>(arr);
+		reverse(arr, 10);
 		print(arr, 0, 9);
 	}
 
@@ -105,9 +134,12 @@ int main() {
 		const char* b = "wor";
 		const char* c = "banana";
 
-		substr<3>(a, b);
-		substr<6>(a, c);
-	}
+//		substr_kmp<3>(a, b);
+//		substr_kmp<6>(a, c);
 
+        substr_brutal(a, b);
+        substr_brutal(a, c);
+	}
+	
 	return 0;
 }
