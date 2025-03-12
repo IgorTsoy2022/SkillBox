9a278908d42bc728c2d340fbe57295fca20eeaab
Igor <97473595+IgorTsoy2022@users.noreply.github.com>
2025-03-12 17:03:53 +0300

    Add files via upload

--- m21_01-04.cpp
+++ m21_01-04.cpp
@@ -170,38 +170,91 @@ std::string date_to_ddmmyyyy(const DATE& date, const char delimeter = '.') {
     return ddmmyyyy;
 }
 
-void write(std::ofstream& file, PAYROLL& p) {
-    int size = p.name.size();
-    file.write((char*)&size, sizeof(size));
-    file.write((char*)p.name.c_str(), size);
+std::vector<PAYROLL> read(const std::string& filename,
+                          std::ostream & stream = std::cout) {
+    std::vector<PAYROLL> paylist;
 
-    auto strdate = date_to_ddmmyyyy(p.date);
-    size = strdate.size();
-    file.write((char*)&size, sizeof(size));
-    file.write((char*)strdate.c_str(), size);
+    std::ifstream file(filename, std::ios::binary);
+    if (file.is_open()) {
+        PAYROLL p{};
+        while (!file.eof()) {
+            int size = 0;
+            file.read((char*)&size, sizeof(size));
+            if (file.eof()) {
+                break;
+            }
+            p.name.resize(size);
+            file.read((char*)p.name.c_str(), size);
 
-    file.write((char*)&p.amount, sizeof(p.amount));
-}
+            file.read((char*)&size, sizeof(size));
+            std::string strdate = "";
+            strdate.resize(size);
+            file.read((char*)strdate.c_str(), size);
+            if (!ddmmyyyy_to_date(strdate, '.', p.date)) {
+                break;
+            }
 
-bool read(std::ifstream& file, PAYROLL& p) {
-    int size = 0;
-    file.read((char*)&size, sizeof(size));    if(file.eof()) {
-        return false;
+            file.read((char*)&p.amount, sizeof(p.amount));
+            if (file.eof()) {
+                break;
+            }
+
+            paylist.push_back(p);
+            stream << p.name << " " << strdate << " " << p.amount << "\n";
+        }
+        file.close();
     }
 
-    p.name.resize(size);
-    file.read((char*)p.name.c_str(), size);
+    return paylist;
+}
 
-    file.read((char*)&size, sizeof(size));
-    std::string strdate = "";
-    strdate.resize(size);
-    file.read((char*)strdate.c_str(), size);
+void write(const std::string& filename, std::vector<PAYROLL> & paylist,
+           std::istream& stream = std::cin) {
+    std::ofstream file(filename, std::ios::app | std::ios::binary);
+    if (file.is_open()) {
+        std::string input;
+        while (std::getline(stream, input)) {
+            if (input == "exit") {
+                break;
+            }
 
-    ddmmyyyy_to_date(strdate, '.', p.date);
+            std::string name = "";
+            std::string lname = "";
+            std::string strdate = "";
+            std::string stramount = "";
 
-    file.read((char*)&p.amount, sizeof(p.amount));
+            std::stringstream input_stream(input);
+            input_stream >> name >> lname >> strdate >> stramount;
 
-    return !file.eof();
+            DATE date{};
+            PAYROLL p{};
+            if (ddmmyyyy_to_date(strdate, '.', date)) {
+                if (is_number(stramount)) {
+                    p.name = name + " " + lname;
+                    int size = p.name.size();
+                    file.write((char*)&size, sizeof(size));
+                    file.write((char*)p.name.c_str(), size);
+
+                    p.date = date;
+                    size = strdate.size();
+                    file.write((char*)&size, sizeof(size));
+                    file.write((char*)strdate.c_str(), size);
+
+                    p.amount = std::stod(stramount);
+                    file.write((char*)&p.amount, sizeof(p.amount));
+
+                    paylist.push_back(p);
+                }
+                else {
+                    std::cout << "Incorrect amount!\n";
+                }
+            }
+            else {
+                std::cout << "Incorrect date!\n";
+            }
+        }
+        file.close();
+    }
 }
 
 void print(const std::vector<PAYROLL>& plist) {
@@ -228,81 +281,103 @@ bool text_view(const std::string& filename) {
     return isOk;
 }
 
-// Task 4. ATM 
+// Task 2. Settlement.
+
+enum class ROOM_TYPE {
+    BEDROOM,
+    LIVING,
+    NURSERY,
+    CABINET,
+    CANTEEN,
+    KITCHEN,
+    BATHROOM,
+    UNKNOWN
+};
+
+struct ROOM {
+    ROOM_TYPE room = ROOM_TYPE::UNKNOWN;
+    std::string name = "";
+    double area = 0.0;
+};
+
+struct FLOOR {
+    int number = 0;
+    double height = 0.0;
+    double area = 0.0;
+    std::vector<ROOM> rooms;
+};
+
+enum class BUILDING_TYPE {
+    HOUSE,
+    GARAGE,
+    LODGE,
+    BATHHOUSE,
+    UNKNOWN
+};
+
+struct BUILDING {
+    BUILDING_TYPE building = BUILDING_TYPE::UNKNOWN;
+    double area = 0.0;
+    std::vector<FLOOR> floors;
+};
+
+struct PLOT {
+    double area = 0.0;
+    std::vector<BUILDING> buildings;
+};
 
 int main() {
 
     std::cout << std::boolalpha;
-
-    std::cout << "Task 1. Entry in the payroll.\n";
+/*
+    std::cout << "Task 1. Payroll.\n";
     {
-        std::cout << "Consecutively enter the rows with the entries : Name and\n";
-        std::cout << "Surname of the payee, date and amount of payment. The\n";
-        std::cout << "data must be separated by spaces (type \"exit\" for exit):\n";
-
-        std::vector<PAYROLL> paylist;
         std::string filename = "payroll_test.bin";
-        std::ofstream file;
-        file.open(filename, std::ios::app | std::ios::binary);
-        if (file.is_open()) {
-            std::string input;
-            while (std::getline(std::cin, input)) {
-                if (input == "exit") {
-                    break;
-                }
+        std::vector<PAYROLL> paylist;
 
-                std::string fname = "";
-                std::string lname = "";
-                std::string strdate = "";
-                std::string amount = "";
-
-                std::stringstream input_stream(input);
-                input_stream >> fname >> lname >> strdate >> amount;
-
-                DATE date{};
-                PAYROLL p{};
-                if (ddmmyyyy_to_date(strdate, '.', date)) {
-                    if (is_number(amount)) {
-                        p.name = fname + " " + lname;
-                        p.date = date;
-                        p.amount = std::stod(amount);
-                        paylist.push_back(p);
-
-                        write(file, p);
+        std::cout << "Type \"list\" to view the statement or \"add\" to add an entry";
+        std::cout << " or \"exit\" for exit: ";
+        bool hasbeenread = false;
+        bool hasbeenwritten = false;
+        std::string input;
+        while (std::getline(std::cin, input)) {
+            if (input == "exit") {
+                if (hasbeenread || hasbeenwritten) {
+                    if (!hasbeenread) {
+                        std::cout << "Records have been added:\n";
                     }
                     else {
-                        std::cout << "Incorrect amount!\n";
+                        std::cout << "File content:\n";
+                    }
+                    if (paylist.empty()) {
+                        std::cout << "Empty.\n";
+                    }
+                    else {
+                        print(paylist);
                     }
                 }
-                else {
-                    std::cout << "Incorrect date!\n";
-                }
-
+                break;
             }
-        }
-        file.close();
-    }
-    {
-        std::vector<PAYROLL> paylist;
-        std::string filename = "payroll_test.bin";
-        std::ifstream file;
-        file.open(filename, std::ios::binary);
-        if (file.is_open()) {
-            PAYROLL p{};
-            while (!file.eof()) {
-                if (read(file, p)) {
-                    paylist.push_back(p);
-                }
+            if (input == "list") {
+                paylist = read(filename);
+                hasbeenread = true;
             }
+            else if (input == "add") {
+                std::cout << "Consecutively enter the rows with the entries : Name and\n";
+                std::cout << "Surname of the payee, date and amount of payment. The\n";
+                std::cout << "data must be separated by spaces (type \"exit\" for exit):\n";
+                write(filename, paylist);
+                hasbeenwritten = true;
+            }
+            std::cout << "Type \"list\" to view the statement or \"add\" to add an entry";
+            std::cout << " or \"exit\" for exit: ";
         }
-
-        file.close();
-        std::cout << "File content:\n";
-        print(paylist);
     }
+//*/
 
-    std::cout << "\nTask 2. Drawing random pictures.\n";
+    std::cout << "\nTask 2. Settlement.\n";
     {
+
     }
 
     std::cout << "\nTask 3. Fishing.\n";
