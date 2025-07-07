b521e485e7ced56f1fef0582bdb05ccdc55d7092
Igor <97473595+IgorTsoy2022@users.noreply.github.com>
2025-07-07 17:04:20 +0300

    Add files via upload

--- QtM37_01_Calculator/calculator.h
+++ QtM37_01_Calculator/calculator.h
@@ -6,21 +6,51 @@
 
 QT_BEGIN_NAMESPACE
 namespace Ui {
-class MainWindow;
+    class MainWindow;
 }
 QT_END_NAMESPACE
 
-class MainWindow : public QMainWindow {
+class MainWindow : public QMainWindow
+{
     Q_OBJECT
 
 public:
-    explicit MainWindow(QWidget *parent = nullptr)
-        : QMainWindow(parent)
-    {}
-
+    MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
+
+private slots:
+    void on_pushButton_AC_clicked();
+    void on_pushButton_BS_clicked();
+    void on_pushButton_percent_clicked();
+    void on_pushButton_divide_clicked();
+    void on_pushButton_multiply_clicked();
+    void on_pushButton_minus_clicked();
+    void on_pushButton_plus_clicked();
+    void on_pushButton_equal_clicked();
+    void on_pushButton_sign_clicked();
+    void on_pushButton_dot_clicked();
+    void on_pushButton_0_clicked();
+    void on_pushButton_1_clicked();
+    void on_pushButton_2_clicked();
+    void on_pushButton_3_clicked();
+    void on_pushButton_4_clicked();
+    void on_pushButton_5_clicked();
+    void on_pushButton_6_clicked();
+    void on_pushButton_7_clicked();
+    void on_pushButton_8_clicked();
+    void on_pushButton_9_clicked();
+
+    void on_lineInput_cursorPositionChanged(int arg1, int arg2);
+    void on_lineInput_editingFinished();
+    void on_lineInput_inputRejected();
+    void on_lineInput_returnPressed();
+    void on_lineInput_selectionChanged();
+    void on_lineInput_textEdited(const QString &arg1);
+    void on_lineInput_textChanged(const QString &arg1);
+
 
 private:
     Ui::MainWindow *ui;
 };
+
 #endif // CALCULATOR_H
--- QtM37_01_Calculator/main.cpp
+++ QtM37_01_Calculator/main.cpp
@@ -1,12 +1,19 @@
 #include <QApplication>
-
+//#include "./ui_calculator.h"
 #include "calculator.h"
 
 
-int main(int argc, char *argv[])
+
+
+int main(int argc, char* argv[])
 {
     QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
-    return a.exec();
+    MainWindow window;
+//    QMainWindow window(nullptr);
+//    Ui::MainWindow calculator;
+//    calculator.setupUi(&window);
+//    window.lineEdit = calculator.lineEdit;
+    window.resize(480, 640);
+    window.show();
+    return QApplication::exec();
 }
