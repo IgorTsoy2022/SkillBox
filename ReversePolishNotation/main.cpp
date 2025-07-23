34308e56cdaf6b738d9b5fe4297c53ba95ac1329
Igor <97473595+IgorTsoy2022@users.noreply.github.com>
2025-07-23 13:34:15 +0300

    Update main.cpp

--- ReversePolishNotation/main.cpp
+++ ReversePolishNotation/main.cpp
@@ -6,13 +6,6 @@ int main() {
 
     std::cout << std::setprecision(26) << std::boolalpha;
 
-    float fPI_ = 3.141592653589793238462643;
-    double dPI_ = 3.141592653589793238462643;
-    long double lPI_ = 3.141592653589793238462643;
-    std::cout << fPI_ << std::endl;
-    std::cout << dPI_ << std::endl;
-    std::cout << lPI_ << std::endl;
-
     std::string input = "";
     std::cout << "Calculator:\n";
     std::cout << "Input math expression (type \"exit\" for exit):\n";
